331800eed243557f614b78e50d9eda8c2b10084c
Jonathan Podkomorka <42620675+Podkomorka@users.noreply.github.com>
2022-10-03 22:22:48 -0400

    Add files via upload

--- src/ArticleDetails.js
+++ src/ArticleDetails.js
@@ -1,6 +1,50 @@
+import { useParams } from "react-router-dom";
+import ArticleData from './articles.json';
+import './article.css';
+import { useEffect } from "react";
+
 const ArticleDetails = () => {
+  const { id } = useParams();
+  const article = ArticleData.find(x => x.id === Number(id));
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+
   return (
-    <div className="">Hi</div>
+    <div className="article-details">
+      <section id="titleDetail">
+        <h1>{article.title}</h1>
+        <div className="details">
+          <p className="author">{article.author}</p>
+          <p>|</p>
+          <p className="date">{article.date}</p>
+          <p>|</p>
+          <div className="categories">
+            {
+              article.categories.map(category => {
+                return(
+                  <p className={`article__category article__category--${category}`}></p>
+                )
+              })
+            }
+          </div>
+        </div>
+      </section>
+
+      <section className="body">
+
+        {/* Loop through array and return all elements */}
+        {
+          article.details.map(element => {
+            return(
+              <div dangerouslySetInnerHTML={ {__html: element} } />
+            )
+          })
+        }
+
+      </section>
+    </div>
   );
 }
  
--- src/Articles.js
+++ src/Articles.js
@@ -8,34 +8,34 @@ const Articles = () => {
 
       <div className="articles">
 
-      {
-        ArticleData.map( article => {
-          return(
-            <div className="article">
-              
-              <img className="article__image" src={require(`./articles/thumbnail${article.id.toString()}.jpg`)} alt="" height="300" width="400"/>
-              <div className="article__content">
-                <div className="article__details">
-                  <p className="article__date">{article.date}</p>
-                  <div className="article__categories">
-                    {
-                      article.categories.map(category => {
-                        return(
-                          <p className={`article__category article__category--${category}`}></p>
-                        )
-                      })
-                    }
+        {
+          ArticleData.map( article => {
+            return(
+              <div className="article">
+                
+                <img className="article__image" src={require(`./articles/thumbnail${article.id.toString()}.jpg`)} alt="" height="300" width="400"/>
+                <div className="article__content">
+                  <div className="article__details">
+                    <p className="article__date">{article.date}</p>
+                    <div className="article__categories">
+                      {
+                        article.categories.map(category => {
+                          return(
+                            <p className={`article__category article__category--${category}`}></p>
+                          )
+                        })
+                      }
+                    </div>
                   </div>
+                  <h3 className="article__title">{article.title}</h3>
+                  <p className="article__intro">{article.intro}</p>
+                  <p className="article__author">{article.author}</p>
                 </div>
-                <h3 className="article__title">#{article.id} - {article.title}</h3>
-                <p className="article__intro">{article.intro}</p>
-                <p className="article__author">{article.author}</p>
+                <Link className="article__link" to={`/articles/${article.id}`}></Link>
               </div>
-              <Link className="article__link" to={`/articles/${article.id}`}></Link>
-            </div>
-          )
-        })
-      }
+            )
+          })
+        }
       </div>
     </div>
   );
