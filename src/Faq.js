de99f1e6d97981f996909cda553a3d9654021ad2
Jonathan Podkomorka <42620675+Podkomorka@users.noreply.github.com>
2023-02-07 22:06:35 -0500

    Add files via upload

--- src/Faq.js
+++ src/Faq.js
@@ -11,7 +11,7 @@ const Faq = () => {
         <div>
           <img 
             className="card"
-            src="https://c1.scryfall.com/file/scryfall-cards/large/front/3/9/396f9198-67b6-45d8-91b4-dc853bff9623.jpg?1652823376"
+            src="https://cards.scryfall.io/normal/front/3/9/396f9198-67b6-45d8-91b4-dc853bff9623.jpg?1674134856"
             alt="Abdel Adrian, Gorion's Ward"
           />
         </div>
@@ -25,7 +25,7 @@ const Faq = () => {
         <div>
           <img
             className="card" 
-            src="https://c1.scryfall.com/file/scryfall-cards/large/front/8/a/8a665794-513f-4f78-92c9-1844ec27c79c.jpg?1595573141" 
+            src="https://cards.scryfall.io/normal/front/8/a/8a665794-513f-4f78-92c9-1844ec27c79c.jpg?1595573141" 
             alt="Oakhame Ranger"
           />
         </div>
